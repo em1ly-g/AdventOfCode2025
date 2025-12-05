6f1c39426b7f6d650a0353965c9c42428b02315c
emilysaboss <emily.gillott2@gmail.com>
2025-12-05 21:11:00 +0000

    Day 2 Task 2

--- Day2/Day2.java
+++ Day2/Day2.java
@@ -67,9 +67,68 @@ public class Day2 {
         return idList;
     }
 
+    private int countOccurences(String string, String subString) {
+        int count = 0;
+        String testString = "";
+        
+        while (testString.length() < string.length()) {
+            testString += subString;
+            if (string.contains(testString)) {
+                count ++;
+                if (testString.equals(string)){
+                    return count;
+                }
+            } else { 
+                // don't care about the count in this scenario
+                return 0;
+            }
+        }
+
+        // dont care about count
+        return 0;
+    }
+
+    private boolean validateId2(String id) {
+        // max substring to be repeated twice
+        int maxSubstring = id.length()/2;
+
+        String substring;
+
+        for (int i=maxSubstring; i>0; i--) {
+            substring = id.substring(0, i);
+            int substringRepititions = countOccurences(id, substring);
+
+            if (substringRepititions > 1) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private void part2(String[] data) {
+        long invalidIdTotal = 0;
+
+        for (String productId : data) {
+            String[] ids = productId.split("-");
+
+            long firstId = Long.valueOf(ids[0]);
+            long lastId = Long.valueOf(ids[1]);
+
+            List<String> idList = getAllStringIds(firstId, lastId);
+
+            for(String id : idList) {
+                if (!validateId2(id)) {
+                   invalidIdTotal += Long.valueOf(id);
+                }
+            }
+        }
+        System.out.println("Part 2: " + invalidIdTotal);
+    }
+
     public static void main(String[] args) {
         Day2 day2 = new Day2();
         String[] data = day2.readFile();
         day2.part1(data);
+        day2.part2(data);
     }
 }
