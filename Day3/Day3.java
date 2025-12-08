3fb977e58c4bce47c0e58bf77939ccf8fa56022b
emilysaboss <emily.gillott2@gmail.com>
2025-12-08 21:47:25 +0000

    Day 3 Task 2

--- Day3/Day3.java
+++ Day3/Day3.java
@@ -2,7 +2,6 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
-import java.util.stream.Collectors;
 
 public class Day3 {
     // String fileName = "Day3/testData.txt";
@@ -71,11 +70,57 @@ public class Day3 {
 
     }
 
+    private int getLargestLocation(List<Integer> list) {
+        int largestValue = -1;
+        int location = -1;
+        for (int i=0; i<list.size(); i++) {
+            if (list.get(i) > largestValue) {
+                largestValue = list.get(i);
+                location = i;
+            }
+        }
+
+        return location;
+    }
+
+    private long getLargestJoltage2(List<Integer> bank) {
+        
+        long joltage = 0;
+
+        for (int i=12; i>0; i--) {
+            List<Integer> subList = bank.subList(0, bank.size() - i + 1);
+            int largestLocation = getLargestLocation(subList);
+
+            joltage = joltage * 10;
+            joltage += subList.get(largestLocation);
+
+            // trim the beginning of the list
+            bank = bank.subList(largestLocation+1, bank.size());
+        }
+
+        return joltage;
+    }
+
+    private void part2(List<List<Integer>> data) {
+
+        List<Long> joltageList = new ArrayList<>();
+
+        for (List<Integer> bank : data) {
+            long joltage = getLargestJoltage2(bank);
+            joltageList.add(joltage);
+        }
+
+        long sum = joltageList.stream().mapToLong(i -> i.longValue()).sum();
+
+        System.out.println("Part 2: " + sum);
+
+    }
+
     public static void main(String[] args) {
         Day3 day3 = new Day3();
         List<List<Integer>> data = day3.readData();
         day3.part1(data);
-
+        day3.part2(data);
     }
     
 }
